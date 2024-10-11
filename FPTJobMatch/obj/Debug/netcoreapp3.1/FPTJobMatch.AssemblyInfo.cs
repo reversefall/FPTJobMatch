8431154df59e56ebbf07b0ec0685cb480a7b6552
reversefall <nphutien.1704@gmail.com>
2024-10-11 16:30:16 +0700

    feat

--- FPTJobMatch/obj/Debug/netcoreapp3.1/FPTJobMatch.AssemblyInfo.cs
+++ FPTJobMatch/obj/Debug/netcoreapp3.1/FPTJobMatch.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPTJobMatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4353d3d10a1e274b29fd92d200fb83fb0d642948")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a89c6a2e3e22ccdfc315af3764b4021005236dfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPTJobMatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPTJobMatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
